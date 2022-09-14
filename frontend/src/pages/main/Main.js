3c77ce3c07eb51d7e43bc3029ea5b3101890afd2
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-09-15 00:09:51 +0900

    Feat: [FE] 버튼 생성 및 절대경로 설정

--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -1,10 +1,11 @@
 // 식사 기록 페이지
 
+import * as React from 'react';
+import  BtnGray  from "components/common/BtnGray";
+
 function Main() {
     return (
-        <div>
-            "Main"
-        </div>
+        <BtnGray size="120px">수정</BtnGray>
     );
 }
   
