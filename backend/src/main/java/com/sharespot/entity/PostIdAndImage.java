aee1de944fddaad6cb52e8a0accec74a6f33b7a0
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-09-14 10:40:07 +0900

    Chore: 테스트 프로젝트 삭제

--- backend/src/main/java/com/sharespot/entity/PostIdAndImage.java
+++
@@ -1,6 +0,0 @@
-package com.sharespot.entity;
-
-public interface PostIdAndImage {
-    Integer getPostId();
-    Integer getImage();
-}
--- backend/src/main/java/com/sharespot/repo/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sharespot.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sharespot.entity.Report;
-
-public interface ReportRepository extends JpaRepository<Report, Integer>{
-
-}
