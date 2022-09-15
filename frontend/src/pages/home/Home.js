500eea0b43a5aca1847d90cc458e200f1c36f168
fridayhs <97663840+fridayhs@users.noreply.github.com>
2022-09-15 10:01:54 +0900

    Add Home.css

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,1 +1,23 @@
-// 메인 페이지
+/*
+메인 페이지
+@author 전상현
+@since 0915-0854
+@최근수정 
+*/
+
+import React from 'react';
+import './Home.css'
+import Card from '@mui/material/Card';
+// import monitor from 'src/assets/monitor.png';
+
+function Home() {
+  return (
+    <div className='bgColor'>
+      <div>
+        {/* <img src={monitor} alt='' /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Home;
