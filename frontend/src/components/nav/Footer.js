b9b913726c8c4b205d68ec8054cc5a3d55c8036a
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-09-16 17:08:15 +0900

    Feat: [FE] components
    헤더, 푸터 완료

--- frontend/src/components/nav/Footer.js
+++ frontend/src/components/nav/Footer.js
@@ -1,1 +1,21 @@
 // footer
+import { Button } from "@mui/material";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+import { Typography } from "@mui/material";
+
+export default function Footer() {
+    return (
+        <div className='footer-container'>
+                <div className="footer-content">
+                    <div className="footer-logo">
+                    <img src="assets/planet.png" width="60" height="50" alt="pill" />
+                        <Typography>PLANEAT</Typography>
+                    </div>
+                    <Typography>Copyright© SSAFY 7th Seoul A701. All rights reserved.</Typography>
+                </div>
+
+        </div>
+    )
+}
