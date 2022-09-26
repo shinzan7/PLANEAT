de51dceba94d609c76653c971c5fa2dd3b154e0a
박윤하 <ha110011@naver.com>
2022-09-26 17:14:39 +0900

    Chore: [BE] AuthInterceptor주석 해제

--- backend/src/main/java/planeat/interceptor/AuthInterceptor.java
+++ backend/src/main/java/planeat/interceptor/AuthInterceptor.java
@@ -1,36 +1,36 @@
-//package planeat.interceptor;
-//
-///*
-// *
-// * AuthInterceptor
-// *
-// @author 박윤하
-// @since 2022-09-19
-//*/
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//import planeat.config.jwt.JwtService;
-//import planeat.exception.CustomException;
-//import planeat.exception.CustomExceptionList;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class AuthInterceptor implements HandlerInterceptor {
-//
-//    private final JwtService jwtService;
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        String accessToken = request.getHeader("accessToken");
-//        if (jwtService.verifyToken(accessToken)) {
-//            return true;
-//        }
-//        throw new CustomException(CustomExceptionList.ACCESS_TOKEN_ERROR);
-//    }
-//
-//}
+package planeat.interceptor;
+
+/*
+*
+* AuthInterceptor
+*
+@author 박윤하
+@since 2022-09-19
+*/
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import planeat.config.jwt.JwtService;
+import planeat.exception.CustomException;
+import planeat.exception.CustomExceptionList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+   private final JwtService jwtService;
+
+   @Override
+   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+       String accessToken = request.getHeader("accessToken");
+       if (jwtService.verifyToken(accessToken)) {
+           return true;
+       }
+       throw new CustomException(CustomExceptionList.ACCESS_TOKEN_ERROR);
+   }
+
+}
