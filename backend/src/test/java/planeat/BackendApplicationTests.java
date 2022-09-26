f7d371f05d8f4bdb6e4a1a4bce0f414f43aec92d
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-09-26 23:00:33 +0900

    Chore: [BE] Test Transactional 추가

--- backend/src/test/java/planeat/BackendApplicationTests.java
+++ backend/src/test/java/planeat/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package planeat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class BackendApplicationTests {
 
 	@Test
