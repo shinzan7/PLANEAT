78fb0d74853fc818f0c0498f107cddede095e1a5
박윤하 <ha110011@naver.com>
2022-09-27 06:16:27 +0900

    Chore: [BE] WebCofig 주석 처리

--- backend/src/main/java/planeat/interceptor/WebConfig.java
+++ backend/src/main/java/planeat/interceptor/WebConfig.java
@@ -1,34 +1,34 @@
-package planeat.interceptor;
+// package planeat.interceptor;
 
-/*
-*
-* WebConfig
-*
-@author 박윤하
-@since 2022-09-19
-*/
+// /*
+// *
+// * WebConfig
+// *
+// @author 박윤하
+// @since 2022-09-19
+// */
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// @EnableWebMvc
+// @RequiredArgsConstructor
+// public class WebConfig implements WebMvcConfigurer {
 
-   private final AuthInterceptor authInterceptor;
+//    private final AuthInterceptor authInterceptor;
 
-   @Override
-   public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
 
-       interceptorRegistry.addInterceptor(authInterceptor)
-               .addPathPatterns("/**")
-               .excludePathPatterns("/oauth/**", "/api/**");
+//        interceptorRegistry.addInterceptor(authInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/oauth/**", "/api/**");
 
-   }
+//    }
 
-}
+// }
 
