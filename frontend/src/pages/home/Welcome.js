a19335f2a3b7d8e484ddf9a165dbcf99ea0e682f
chohyean <ashley3053@gmail.com>
2022-09-26 10:24:48 +0900

    Chore: [FE] 이용약관 체크이벤트 수정

--- frontend/src/pages/home/Welcome.js
+++ frontend/src/pages/home/Welcome.js
@@ -78,7 +78,17 @@ function Welcome() {
   const [open, setOpen] = React.useState(false);
 
   const handleNext = () => {
-    setActiveStep(activeStep + 1);
+    // 약관동의 화면
+    if (activeStep == 0) {
+      // 약관동의 체크 되어있으면 다음 스텝으로 이동, 체크 안되어있으면 alert
+      if (checked) {
+        setActiveStep(activeStep + 1);
+      } else {
+        setOpen(true);
+      }
+    } else {
+      setActiveStep(activeStep + 1);
+    }
   };
 
   const handleClose = () => {
