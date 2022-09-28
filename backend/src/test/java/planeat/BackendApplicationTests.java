5d3f742c9a2b387f04769cc7dcfb1517fb0df9a4
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-09-28 15:14:59 +0900

    Chore: [BE] 테스트파일 주석

--- backend/src/test/java/planeat/BackendApplicationTests.java
+++ backend/src/test/java/planeat/BackendApplicationTests.java
@@ -1,15 +1,15 @@
-package planeat;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@Transactional
-class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package planeat;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//class BackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- backend/src/test/java/planeat/database/repository/NutrientIngredientRespositoryTest.java
+++ backend/src/test/java/planeat/database/repository/NutrientIngredientRespositoryTest.java
@@ -1,32 +1,32 @@
-package planeat.database.repository;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-import planeat.database.entity.NutrientIngredient;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@Transactional
-class NutrientIngredientRespositoryTest {
-    @Autowired
-    NutrientIngredientRespository nutrientIngredientRespository;
-
-    @Test
-    public void test1(){
-        Optional<NutrientIngredient> ni = nutrientIngredientRespository.findById(1L);
-        System.out.println(ni.get().getIngredientAmount());
-    }
-
-    @Test
-    public void testfindAllbyNutrientId(){
-        List<NutrientIngredient> allByNutrientId = nutrientIngredientRespository.findAllByNutrientId(5L);
-        System.out.println(allByNutrientId.get(0).getIngredientAmount());
-    }
-
-}
+//package planeat.database.repository;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//import planeat.database.entity.NutrientIngredient;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@Transactional
+//class NutrientIngredientRespositoryTest {
+//    @Autowired
+//    NutrientIngredientRespository nutrientIngredientRespository;
+//
+//    @Test
+//    public void test1(){
+//        Optional<NutrientIngredient> ni = nutrientIngredientRespository.findById(1L);
+//        System.out.println(ni.get().getIngredientAmount());
+//    }
+//
+//    @Test
+//    public void testfindAllbyNutrientId(){
+//        List<NutrientIngredient> allByNutrientId = nutrientIngredientRespository.findAllByNutrientId(5L);
+//        System.out.println(allByNutrientId.get(0).getIngredientAmount());
+//    }
+//
+//}
