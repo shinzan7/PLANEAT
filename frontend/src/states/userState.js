86c17e9818f3f1fcb0707fce385da731ae64d8d9
fridayhs <97663840+fridayhs@users.noreply.github.com>
2022-09-28 22:16:49 +0900

    Feat:[FE] recoil test

---
+++ frontend/src/states/userState.js
@@ -0,0 +1,17 @@
+import { atom } from 'recoil';
+
+  const accessToken = localStorage.getItem('accessToken')
+  const refreshToken = localStorage.getItem('refreshToken')
+  const accessTokenExpiration = localStorage.getItem('accessTokenExpiration')
+  const refreshTokenExpiration = localStorage.getItem('refreshTokenExpiration')
+  const userId = localStorage.getItem('userId')
+  const name = localStorage.getItem('name')
+  const birthYear = localStorage.getItem('birthYear')
+  const gender = localStorage.getItem('gender')
+
+export const userState = atom({
+  key: 'user',
+  default: [ {accessToken}, {refreshToken}, {accessTokenExpiration}, {refreshTokenExpiration}, 
+  {userId}, {name}, {birthYear}, {gender} ],
+  
+}) 
