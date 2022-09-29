fe27ff9f3e5172f2fcd80f879303efd4939442ba
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-09-30 00:59:13 +0900

    Fix: [BE] JPQL param 추가

--- backend/src/main/java/planeat/database/repository/NutrientHistoryRepository.java
+++ backend/src/main/java/planeat/database/repository/NutrientHistoryRepository.java
@@ -3,6 +3,7 @@ package planeat.database.repository;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import planeat.database.entity.NutrientHistory;
 
 import java.util.List;
@@ -11,6 +12,6 @@ public interface NutrientHistoryRepository extends JpaRepository<NutrientHistory
 
     @EntityGraph(attributePaths = {"userNutrient"})
     @Query("select n from NutrientHistory n where n.userNutrient.id = :userNutrientId")
-    List<NutrientHistory> findAllByUserNutrientId(Long userNutrientId);
+    List<NutrientHistory> findAllByUserNutrientId(@Param("userNutrientId") Long userNutrientId);
 
 }
