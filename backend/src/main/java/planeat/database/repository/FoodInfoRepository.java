1c0c7cd2b9e31d8e8413d1f74f7e51628fcebdab
Yunha <ha110011@naver.com>
2022-09-29 10:48:06 +0900

    Fix: [BE] FoodInfo API 조회 수정
    FoodInfo API 조회 유저 아이디로 조회할 수 있도록 수정했습니다.

--- backend/src/main/java/planeat/database/repository/FoodInfoRepository.java
+++ backend/src/main/java/planeat/database/repository/FoodInfoRepository.java
@@ -18,11 +18,8 @@ import java.util.Optional;
 
 public interface FoodInfoRepository extends JpaRepository<FoodInfo, Long> {
 
-    @Query("select f from FoodInfo f where f.foodUser = :userId or f.foodUser = 1 order by f.name asc")
-    List<FoodInfo> findAllFoodInfo(Long userId, Pageable pageable);
-
     @Query("select f from FoodInfo f where f.foodUser = :userId")
-    List<FoodInfo> findByUserIdFoodInfo(Long userId);
+    List<FoodInfo> findByUserIdFoodInfo(Long userId, Pageable pageable);
 
     @Query("select f from FoodInfo f where f.name like concat('%', :name, '%') and f.foodUser = :userId or f.foodUser = 1")
     List<FoodInfo> findByNameAndUserIdFoodInfo(String name, Long userId);
