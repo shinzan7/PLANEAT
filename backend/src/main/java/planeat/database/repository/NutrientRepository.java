107b12ab8ea21eb5009c16a4526055c2f0072752
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-09-30 17:00:27 +0900

    Feat: [BE] 영양제 영양소, 태그검색 추가
    네이티브 쿼리로 검색api 기능을 구현함

--- backend/src/main/java/planeat/database/repository/NutrientRepository.java
+++ backend/src/main/java/planeat/database/repository/NutrientRepository.java
@@ -7,7 +7,9 @@ package planeat.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import planeat.api.dto.nutrient.NutrientDto;
+import planeat.api.dto.nutrient.NutrientSearchResponse;
 import planeat.database.entity.Nutrient;
 
 import java.util.List;
@@ -15,9 +17,6 @@ import java.util.Optional;
 
 public interface NutrientRepository extends JpaRepository<Nutrient, Long> {
     Optional<Nutrient> findById(Long id);
-
-    List<Nutrient> findAllByNutrientNameContains(String searchWord);
-
     List<Nutrient> findAll();
 
     @Query("select new planeat.api.dto.nutrient.NutrientDto(n.id, n.nutrientName) from Nutrient n")
