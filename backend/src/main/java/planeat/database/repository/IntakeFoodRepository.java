9a6c1beaf605c2dc3f1e61bd0c1eabc905a21039
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-10-02 22:40:31 +0900

    Feat: [BE] fetch join 추가
    섭취음식 가져올때 음식정보도 같이 가져오도록 수정

--- backend/src/main/java/planeat/database/repository/IntakeFoodRepository.java
+++ backend/src/main/java/planeat/database/repository/IntakeFoodRepository.java
@@ -2,13 +2,12 @@ package planeat.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import planeat.database.entity.IntakeFood;
 
 import java.util.List;
 
 public interface IntakeFoodRepository extends JpaRepository<IntakeFood, Long> {
-
-    @Query("select i from IntakeFood i where i.intakeHistory.id = :id")
-    List<IntakeFood> findByIntakeHistoryId(Long id);
-
+    @Query("select i from IntakeFood i join fetch i.foodInfo where i.intakeHistory.id = :id")
+    List<IntakeFood> findByIntakeHistoryId(@Param("id") Long id);
 }
