fe2546765e9c8c10635d4b2b22f2a6579f894444
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-10-02 22:43:29 +0900

    Fix: [BE] @Param 추가

--- backend/src/main/java/planeat/database/repository/FoodInfoRepository.java
+++ backend/src/main/java/planeat/database/repository/FoodInfoRepository.java
@@ -11,6 +11,7 @@ package planeat.database.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import planeat.database.entity.FoodInfo;
 
 import java.util.List;
@@ -19,13 +20,13 @@ import java.util.Optional;
 public interface FoodInfoRepository extends JpaRepository<FoodInfo, Long> {
 
     @Query("select f from FoodInfo f where f.foodUser = :userId or f.foodUser = 1 order by f.id asc")
-    List<FoodInfo> findAllFoodInfo(Long userId, Pageable pageable);
+    List<FoodInfo> findAllFoodInfo(@Param("userId") Long userId, Pageable pageable);
 
     @Query("select f from FoodInfo f where f.foodUser = :userId")
-    List<FoodInfo> findByUserIdFoodInfo(Long userId, Pageable pageable);
+    List<FoodInfo> findByUserIdFoodInfo(@Param("userId") Long userId, Pageable pageable);
 
     @Query("select f from FoodInfo f where f.name like concat('%', :name, '%') and (f.foodUser = :userId or f.foodUser = 1)")
-    List<FoodInfo> findByNameAndUserIdFoodInfo(String name, Long userId, Pageable pageable);
+    List<FoodInfo> findByNameAndUserIdFoodInfo(@Param("name") String name,@Param("userId") Long userId, Pageable pageable);
 
     Optional<FoodInfo> findById(Long foodInfoId);
 }
