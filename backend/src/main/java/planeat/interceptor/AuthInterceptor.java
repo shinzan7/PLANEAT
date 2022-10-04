3e20b2a220539b0604d23104c0ce08eb7af39a3c
Yunha <ha110011@naver.com>
2022-10-05 07:55:29 +0900

    Fix: [BE] 인터셉터 관련 주석
    인터셉터 관련 주석

--- backend/src/main/java/planeat/interceptor/AuthInterceptor.java
+++ backend/src/main/java/planeat/interceptor/AuthInterceptor.java
@@ -1,47 +1,43 @@
-package planeat.interceptor;
-
- /*
- *
- * AuthInterceptor
- *
- @author 박윤하
- @since 2022-09-19
- */
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import planeat.config.jwt.JwtService;
-import planeat.exception.CustomException;
-import planeat.exception.CustomExceptionList;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Component
-@RequiredArgsConstructor
-public class AuthInterceptor implements HandlerInterceptor {
-
-    private final JwtService jwtService;
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        String url = request.getRequestURI();
-        if (url.contains("swagger") || url.contains("api-docs") || url.contains("webjars")) {
-            return true;
-        }
-
-        String accessToken = request.getHeader("accessToken");
-        System.out.println();
-        System.out.println("================================");
-        System.out.println(accessToken);
-        System.out.println("================================");
-        System.out.println();
-        if (jwtService.verifyToken(accessToken)) {
-            return true;
-        }
-        throw new CustomException(CustomExceptionList.ACCESS_TOKEN_ERROR);
-    }
-
-}
+//package planeat.interceptor;
+//
+// /*
+// *
+// * AuthInterceptor
+// *
+// @author 박윤하
+// @since 2022-09-19
+// */
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import planeat.config.jwt.JwtService;
+//import planeat.exception.CustomException;
+//import planeat.exception.CustomExceptionList;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class AuthInterceptor implements HandlerInterceptor {
+//
+//    private final JwtService jwtService;
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//        String url = request.getRequestURI();
+//        if (url.contains("swagger") || url.contains("api-docs") || url.contains("webjars")) {
+//            return true;
+//        }
+//
+//        String accessToken = request.getHeader("accessToken");
+//        System.out.println();
+//        if (jwtService.verifyToken(accessToken)) {
+//            return true;
+//        }
+//        throw new CustomException(CustomExceptionList.ACCESS_TOKEN_ERROR);
+//    }
+//
+//}
