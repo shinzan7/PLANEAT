0e9f026091ffdeb02c645d4b3a65280578aadad6
Yunha <ha110011@naver.com>
2022-10-06 23:46:30 +0900

    Fix: [BE] 원상 복구...
    원상 복구...

--- backend/src/main/java/planeat/config/SwaggerConfig.java
+++ backend/src/main/java/planeat/config/SwaggerConfig.java
@@ -10,14 +10,12 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Configuration
-@EnableSwagger2
-//@EnableWebMvc
+@EnableWebMvc
 public class SwaggerConfig {
 
     private ApiInfo swaggerInfo() {
