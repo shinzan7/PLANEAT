663d5f9042ffcfcc6a88c8e5554575252c8746a9
Yunha <ha110011@naver.com>
2022-10-06 23:40:57 +0900

    Fix: [BE] 액세스 토큰 재발급 디버깅 중
    액세스 토큰 재발급 디버깅 중

--- backend/src/main/java/planeat/interceptor/WebConfig.java
+++ backend/src/main/java/planeat/interceptor/WebConfig.java
@@ -1,34 +1,40 @@
-//package planeat.interceptor;
-//
-// /*
-// *
-// * WebConfig
-// *
-// @author 박윤하
-// @since 2022-09-19
-// */
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
+package planeat.interceptor;
+
+ /*
+ *
+ * WebConfig
+ *
+ @author 박윤하
+ @since 2022-09-19
+ */
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
 //@EnableWebMvc
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 //public class WebConfig implements WebMvcConfigurer {
-//
-//    private final AuthInterceptor authInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
-//
-//        interceptorRegistry.addInterceptor(authInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/oauth/**");
-//
-//    }
-//
-//}
-//
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+
+        interceptorRegistry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/oauth/**");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+
+}
+
